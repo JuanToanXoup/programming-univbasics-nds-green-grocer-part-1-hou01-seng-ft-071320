8556ac0785982fc8c205051965dc1df03294174b
John Doan <johndoan42@gmail.com>
2020-07-09 21:00:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -24,7 +24,7 @@ unconsolidated_cart = [
 
 def consolidate_cart(cart)
   full_cart = cart.each_with_object([]) do |index,cart_array|
-    if cart_array.include(index)
+    if cart_array.include?(index)
       binding.pry
     end
   end
